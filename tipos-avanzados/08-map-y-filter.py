f0efc57edcba00c5790bab14c9f3322cb41424f5
Arturo Valdez <santiguardado27@gmail.com>
2024-06-30 10:53:23 -0600

    Map - filter

---
+++ tipos-avanzados/08-map-y-filter.py
@@ -0,0 +1,13 @@
+usuarios = [
+    ["Chanchito", 4], 
+    ["Prdito", 1], 
+    ["Paquillo", 5]
+    ]
+
+
+nombre = list(map(lambda usuario: usuario[0], usuarios))
+print(nombre)#['Chanchito', 'Prdito', 'Paquillo']
+
+
+menosUsuarios = list(filter(lambda usuario : usuario[1]>2, usuarios))
+print(menosUsuarios)#[['Chanchito', 4], ['Paquillo', 5]]
