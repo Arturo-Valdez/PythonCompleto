9095c047164c9bc227656ae4b135b79acb55d419
Arturo Valdez <santiguardado27@gmail.com>
2024-07-03 18:32:45 -0600

    Filas

---
+++ tipos-avanzados/13-filas.py
@@ -0,0 +1,20 @@
+# TODO LAS FILAS LLEVAN EL NOBRE FIFO
+#FI -> FIRST IN  // PRIMERO ENTRA
+#FO -> FIRST OUT // PRIMERO SALE
+
+from collections import deque
+
+fila = deque([1,2])
+#agregar
+# fila.append(3)
+# fila.append(4)
+# fila.append(5)
+# print(fila) #deque([1, 2, 3, 4, 5])
+# eliminar
+fila.popleft()
+fila.popleft()
+
+if not fila:
+    print("fila vacia")
+
+print(fila)
