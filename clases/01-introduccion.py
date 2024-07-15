097dc570d492c30892892230183d6952ab41ae03
Arturo Valdez <santiguardado27@gmail.com>
2024-07-15 16:39:09 -0600

    Introduccion CLASES

---
+++ clases/01-introduccion.py
@@ -0,0 +1,12 @@
+mensaje = "Hola mundo"
+print(type(mensaje))#<class 'str'>
+
+
+#CLASE: Es el plano de construccion
+#OBJETO: Es una instancia de una clase
+
+#CLASES: el plano de construccion de una casa
+#OBJETO: La casa construida
+
+#CLASE: Humano
+#OBJETO : Nicolas, Maria, Juan, Carlos
