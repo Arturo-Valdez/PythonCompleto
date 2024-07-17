94e7413f74df532ea3baad5c960808d174f2e929
Arturo Valdez <santiguardado27@gmail.com>
2024-07-17 17:28:52 -0600

    FACTORIAL

---
+++ control-flujo/10-ejercicio_factorial.py
@@ -0,0 +1,18 @@
+# Factorial es un valor que se multiplica por su valor anterior
+# 5! = 5 * 4 * 3 * 2 * 1
+
+def factorial(valor):
+    if valor == 0 or valor == 1:
+        return 1
+    elif valor < 0:
+        return "Solo colocar valores positivos"
+    else:
+        return valor * factorial(valor - 1)
+
+print(factorial(5))#120
+
+print(factorial(-5))#"Solo colocar valores positivos"
+
+print(factorial(0))#1
+
+print(factorial(1))#1
