cc423bc585f4ae2eabe9de8c5d6fc44bfab8bbd6
Arturo Valdez <santiguardado27@gmail.com>
2024-07-21 14:35:50 -0600

    Decorador de properties

--- clases/07-decorador-properties.py
+++ clases/07-decorador-properties.py
@@ -14,14 +14,15 @@ class Perro:
     @nombre.setter
     def nombre(self, nombre):
         print("Este es un Setter")
-        #si nombre esta sin espacios eliminarlos
+        #evalúa si después de eliminar los espacios en blanco, la cadena nombre resultante no es vacía.
+        # Si la cadena no es vacía (es decir, tiene al menos un carácter visible), el cuerpo del if se ejecutará.
         if nombre.strip():
             self.__nombre = nombre
         return 
     
     
 
-perro = Perro("Bruno")
+perro = Perro("Bruno colita de algodon")
 #Este es un Setter
 # Este es un getter
 # Bruno
