3bbe04ca5e91ea83990ac3b1de11df0c1314b103
Arturo Valdez <santiguardado27@gmail.com>
2024-09-10 18:00:48 -0600

    Excepciones-Else- Finally

--- 06-excepciones/02-tipos-excepciones.py
+++ 06-excepciones/02-tipos-excepciones.py
@@ -2,4 +2,10 @@ try:#PROBAR
     n1 = int(input("Dame un numero: "))
 
 except Exception as e:
-    print(type(e))    
+    print("Ocurrio un error")     
+
+else:
+    print("No ocurrio ningun error")
+
+finally:
+    print("Se ejecuta siempre!")
