06d285507235d35caba9552f7b6e1cb6effb9f3e
Arturo Valdez <santiguardado27@gmail.com>
2024-09-21 12:15:40 -0600

    Inyeccion de dependencias

---
+++ 08-rutas/03-inyeccion-dependencias.py
@@ -0,0 +1,19 @@
+# class Perro:
+#     def __init__(self, Correa):
+#         self.correa = Correa()
+
+
+# import Usuario
+
+# def guardar():
+#     Usuario.guardar()
+
+# #INYECCION DE DEPENDENCIAS
+# # La inyección de dependencias es un patrón de diseño que permite gestionar las dependencias de un objeto de manera que no esté acoplado a implementaciones específicas. Esto mejora la flexibilidad, la testabilidad y la mantenibilidad del código.
+# def guardar(entidad):
+#     entidad.guardar()
+
+
+# ejemplo con Flask
+# def init_app(bbdd, api):
+    #inicializacion de modulo
